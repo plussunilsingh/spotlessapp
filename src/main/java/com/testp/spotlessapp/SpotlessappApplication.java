91516474204b84bda4dedcf0ecacec42406ece5f
sunil_tomar <sunil_tomar@epam.com>
2025-08-07 23:04:50 +0530

    initial commit.

---
+++ src/main/java/com/testp/spotlessapp/SpotlessappApplication.java
@@ -0,0 +1,13 @@
+package com.testp.spotlessapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotlessappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotlessappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testp/spotlessapp/SpotlessappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testp.spotlessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotlessappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
